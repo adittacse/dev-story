45f7251d5506da7a97a91de15d8f1137f9d0ff3c
Aditta Chakraborty <cseaditta@gmail.com>
2025-12-19 01:32:39 +0600

    navbar added

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Title from "@/components/Title";
+import Link from "next/link";
 
 const About = () => {
     return (
         <div>
             <Title>About Page</Title>
+
+            <nav className="space-x-5">
+                <Link href="/about/contact">Contact</Link>
+                <Link href="/about/teams">teams</Link>
+            </nav>
         </div>
     );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import Link from "next/link";
 
 const Header = () => {
     return (
-        <header className="px-4 py-3 border-b-2 border-gray-500">
-            <h2 className="text-lg font-semibold">Dev Story</h2>
+        <header className="px-4 py-3 border-b-2 border-gray-500 flex flex-wrap justify-between items-center">
+            <Link href="/" className="text-lg font-semibold">Dev Story</Link>
+
+            <nav className="space-x-5">
+                <Link href="/about">About</Link>
+                <Link href="/tutorials">Tutorials</Link>
+                <Link href="/dashboard">Dashboard</Link>
+                <Link href="/login">Login</Link>
+                <Link href="/register">Register</Link>
+            </nav>
         </header>
     );
 };
