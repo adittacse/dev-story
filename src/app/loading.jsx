e86d2e4e21f0100c79a32bca0872155acd46a78f
Aditta Chakraborty <cseaditta@gmail.com>
2025-12-20 02:24:46 +0600

    loading page added

---
+++ src/app/loading.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Loading = () => {
+    return (
+        <div className="min-h-screen flex justify-center items-center text-8xl">
+            <h2>L</h2>
+            <span className="animate-spin">🌀</span>
+            <h2>ADING</h2>
+        </div>
+    );
+};
+
+export default Loading;
